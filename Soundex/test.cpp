49e54bfd4737819f69333346fc8584bba683ffc6
张伟 冯 <fengzhangweiwei@gmail.com>
2020-03-05 20:16:56 +0800

    初始化

---
+++ Soundex/test.cpp
@@ -0,0 +1,21 @@
+#include "gtest/gtest.h"
+
+TEST(TestCaseName, TestName1) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, TestName2) {
+    EXPECT_EQ(1, 2);
+    EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, TestName3) {
+    EXPECT_EQ(1, 2);
+    EXPECT_TRUE(true);
+}
+
+TEST(TestCaseName, TestName4) {
+    EXPECT_EQ(1, 2);
+    EXPECT_TRUE(true);
+}
